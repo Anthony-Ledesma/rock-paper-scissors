6da98f349cd43dbcbb49a07801a415c06b91afac
Anthony Ledesma <165110055+Anthony-Ledesma@users.noreply.github.com>
2024-06-10 14:27:31 -0700

    Create getHumanChoice function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-console.log('hello world');
-
 function getComputerChoice() {
-    let ranNum = Math.floor((Math.random() * 6))
+    let ranNum = Math.floor((Math.random() * 6));
     console.log(ranNum)
     if (ranNum <= 1) {
         return 'rock';
@@ -9,5 +7,10 @@ function getComputerChoice() {
         return 'paper';
     } else {
         return 'scissors';
-    }
+    };
+};
+
+function getHumanChoice() {
+    let choice = prompt('What is your choice? Rock, Paper or Scissors.');
+    return choice.toLowerCase();
 };
