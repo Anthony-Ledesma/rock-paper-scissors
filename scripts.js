35e2e71a387fdd0777ce3dff15b23a932a0039b4
Anthony Ledesma <165110055+Anthony-Ledesma@users.noreply.github.com>
2024-06-11 10:51:22 -0700

    Create playGame function
    game is now playable and works as expected

--- scripts.js
+++ scripts.js
@@ -14,21 +14,43 @@ function getHumanChoice() {
     return choice.toLowerCase();
 };
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    let a = getHumanChoice();
-    let b = getComputerChoice();
-    console.log('You chose ' + a + ', computer chose ' + b);
-    if (a === b) {
-        return "It's a tie, no points."
-    } else if (a === 'rock' && b === 'scissors' || a === 'paper' && b === 'rock' || a === 'scissors' && b === 'paper') {
-        ++humanScore;
-        return 'You gain a point! The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore
-    } else {
-        ++computerScore;
-        return 'Computer gains a point. The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore;
-    };
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound() {
+        let a = getHumanChoice();
+        let b = getComputerChoice();
+        if (a === b) {
+            console.log('You chose ' + a + ', computer chose ' + b + ". It's a tie, no points.")
+        } else if (a === 'rock' && b === 'scissors' || a === 'paper' && b === 'rock' || a === 'scissors' && b === 'paper') {
+            ++humanScore;
+            console.log('You chose ' + a + ', computer chose ' + b + '. You gain a point! The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore)
+        } else {
+            ++computerScore;
+            console.log('You chose ' + a + ', computer chose ' + b + '. Computer gains a point. The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore);
+        };
+    }
+
+    //Round one
+    playRound();
+
+    //Round two
+    playRound();
+
+    //Round three
+    playRound();
+
+    //Round four
+    playRound();
+
+    //Round five
+    playRound();
+
+    let outcome = (computerScore > humanScore) ? console.log("You lose! Scoreboard: You - " + humanScore + " // computer - " + computerScore) 
+    : (computerScore === humanScore) ? console.log("It's a tie! Scoreboard: You - "+ humanScore + " // computer - " + computerScore) 
+    : console.log("You win! Scoreboard: You - " + humanScore + " // computer - " + computerScore);
+    return outcome;
 }
