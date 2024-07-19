a420f8b569bfd7557b072b078a09934a7bc3c0f2
Anthony Ledesma <165110055+Anthony-Ledesma@users.noreply.github.com>
2024-07-19 13:21:14 -0700

    Add event listener to buttons
    added event listener using a case switch, also created variables to
    store query selectors for human and computer scores

--- scripts.js
+++ scripts.js
@@ -21,7 +21,6 @@ function playGame() {
     let computerScore = 0;
 
     function playRound() {
-        let a = getHumanChoice();
         let b = getComputerChoice();
         if (a === b) {
             console.log('You chose ' + a + ', computer chose ' + b + ". It's a tie, no points.")
@@ -33,24 +32,67 @@ function playGame() {
             console.log('You chose ' + a + ', computer chose ' + b + '. Computer gains a point. The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore);
         };
     }
-
-    //Round one
-    playRound();
-
-    //Round two
-    playRound();
-
-    //Round three
-    playRound();
-
-    //Round four
-    playRound();
-
-    //Round five
-    playRound();
-
     let outcome = (computerScore > humanScore) ? console.log("You lose! Scoreboard: You - " + humanScore + " // computer - " + computerScore) 
     : (computerScore === humanScore) ? console.log("It's a tie! Scoreboard: You - "+ humanScore + " // computer - " + computerScore) 
     : console.log("You win! Scoreboard: You - " + humanScore + " // computer - " + computerScore);
     return outcome;
-}
+    };
+
+
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    let humanCounter = document.querySelector('span#human-score');
+    let computerCounter = document.querySelector('span#computer-score');
+    humanCounter.textContent = humanScore;
+
+    function playRound() {
+        let b = getComputerChoice();
+        if (a === b) {
+            console.log('You chose ' + a + ', computer chose ' + b + ". It's a tie, no points.")
+        } else if (a === 'rock' && b === 'scissors' || a === 'paper' && b === 'rock' || a === 'scissors' && b === 'paper') {
+            ++humanScore;
+            console.log('You chose ' + a + ', computer chose ' + b + '. You gain a point! The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore)
+        } else {
+            ++computerScore;
+            console.log('You chose ' + a + ', computer chose ' + b + '. Computer gains a point. The scoreboard is: You - ' + humanScore + ' // computer - ' + computerScore);
+        };
+    };
+
+
+    let choice = document.querySelector('#choice');
+
+    choice.addEventListener('click', (event) => {
+        let target = event.target;
+    
+        switch(target.id) {
+            case 'rock':
+                a = 'rock';
+                playRound();
+                break;
+            case 'paper':
+                a = 'paper';
+                playRound();
+                break;
+            case 'scissors':
+                a = 'scissors';
+                playRound();
+                break;
+        }
+     });
+   
+    //Round one
+   // playRound();
+
+    //Round two
+   // playRound();
+
+    //Round three
+   // playRound();
+
+    //Round four
+   // playRound();
+
+    //Round five
+   // playRound();
